cb8e9bbc0ddebb14d9bb2eb757302b08f271f797
Juliet Oh <julietoh@nat-oitwireless-inside-vapornet100-b-11257.Princeton.EDU>
2018-04-04 21:13:47 -0400

    Postgres init

--- planprinceton/settings.py
+++ planprinceton/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'planprinceton.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'planprincetondb',
+        'USER': 'ppuser',
+        'PASSWORD': '333',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
