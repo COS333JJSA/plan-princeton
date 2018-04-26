20b17b83f935e2fba295fec2abe8f0f2580f830f
Ayushi Sinha <ayushisinha@nat-tvwna-inside-visitornet101-c-10433.princeton.org>
2018-04-25 22:04:25 -0400

    ayushi static content

---
+++ home/static/plan-princeton/home/static/plan-princeton/home/static/plan-princeton/home/static/plan-princeton/home/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Req_List, Contact, URL, Department, Professor, Listing, Class, Course, Area
+
+admin.site.register(Req_List)
+# admin.site.register(Concentration)
+admin.site.register(Contact)
+admin.site.register(URL)
+admin.site.register(Department)
+admin.site.register(Professor)
+admin.site.register(Listing)
+admin.site.register(Class)
+admin.site.register(Course)
+admin.site.register(Area)
