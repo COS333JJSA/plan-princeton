ba32621061b4596c441e9cb81afb703877313c57
Juliet Oh <julietoh@nat-oitwireless-inside-vapornet100-b-11257.Princeton.EDU>
2018-05-02 14:44:19 -0400

    Login view

--- home/views.py
+++ home/views.py
@@ -2,17 +2,24 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
 	return render(
    	    request,
         'index.html',
     )
 
-@login_required
+
 def login(request):
 	return render(
 		request,
 		'login.html',
 	)
 
+@login_required
+def schedule(request):
+	return render(
+		request,
+		'schedule.html'
+	)
 
