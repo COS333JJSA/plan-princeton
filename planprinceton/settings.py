a0604d3405e1d77de010820673df4a8a120e0374
SLmac <slemkemeier@gmail.com>
2018-05-11 11:40:18 -0400

    deleted django extensions

--- planprinceton/settings.py
+++ planprinceton/settings.py
@@ -50,8 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'planprinceton',
-    'django_cas_ng',
-    'django_extensions'
+    'django_cas_ng'
 ]
 
 MIDDLEWARE_CLASSES = [
